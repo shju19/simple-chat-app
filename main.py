ecb8a15dc155a75c1db4d2a4532ccb548cf6ece4
Sangho <shju19@gmail.com>
2025-04-19 15:46:41 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# main.py
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "response": ""})
+
+@app.post("/chat", response_class=HTMLResponse)
+async def chat(request: Request, user_input: str = Form(...)):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": user_input}]
+    )
+    reply = response["choices"][0]["message"]["content"]
+    return templates.TemplateResponse("index.html", {"request": request, "response": reply})
